60c97566720f3e15b8d0ffb1ae8e27ce1524aebb
Haruka <hfujii1013@gmail.com>
2023-03-22 14:38:51 -0700

    restructured directories

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { xRapidAPIHost, xRapidAPIKey } from "./config";
+
+const CLIENT_ID = xRapidAPIKey;
+const CLIENT_SECRET = xRapidAPIHost;
+
+function App() {
+  const [serchInput, setSerchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    //API Access Token
+    var authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => setAccessToken(data.access_token));
+  }, []);
+
+  //Search
+  async function search() {
+    console.log("Serch for " + serchInput); // Taylor Swift
+
+    //Get request using search to get Artist ID
+    var searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    var artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + serchInput + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    console.log("Artist ID is " + artistID);
+
+    //Get request with Artist ID grab all the albums from artist
+    var returnedAlbums = await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAlbums(data.items);
+      });
+
+    //Display those albums to the user
+  }
+  console.log(albums);
+
+  return (
+    <div className="App">
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Serch For Artist"
+            type="input"
+            onKeyPress={(event) => {
+              if (event.key == "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSerchInput(event.target.value)}
+          />
+          <Button onClick={search}>Serch</Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map((album, i) => {
+            console.log(album);
+            return (
+              <Card>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
